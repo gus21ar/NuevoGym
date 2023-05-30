63650b491f7dd2c42b2856b38ad51bee38d85fef
Gustavo Vicente Delponte <gus21ar@hotmail.com>
2023-05-30 20:02:49 -0300

    Diccionarios

--- NuevoGym/ViewModels/MainWindowViewModel.cs
+++ NuevoGym/ViewModels/MainWindowViewModel.cs
@@ -4,7 +4,7 @@ namespace NuevoGym.ViewModels
 {
 	public class MainWindowViewModel : BindableBase
 	{
-		private string _title = "Prism Application";
+		private string _title = "GymCheck Wpf";
 		public string Title
 		{
 			get { return _title; }
